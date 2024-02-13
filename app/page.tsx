ea2ddf0ea4830d0a01634a5c7db8f9271c7fda2f
codeph-0bia <codephobia642@gmail.com>
2024-02-13 14:41:20 +0530

    🔨: feat() line width

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,13 @@ import { ChromePicker } from 'react-color';
 
 export default function Home() {
   const [color, setColor] = useState<string>('#000');
+  const [lineWidth, setLineWidth] = useState<number>(5);
   const { canvasRef, onMouseDown, clear } = useDraw(drawLine);
 
   function drawLine({ prevPoint, currentPoint, ctx }: Draw) {
     const { x: currX, y: currY } = currentPoint;
     const lineColor = color;
-    const linewidth = 5;
+    const linewidth = lineWidth;
 
     let startPoint = prevPoint ?? currentPoint;
     ctx.beginPath();
@@ -30,6 +31,15 @@ export default function Home() {
   return (
     <div className="w-screen h-screen bg-white flex justify-center items-center">
       <div className="flex flex-col gap-10 pr-10">
+        <div className="flex flex-col gap-2">
+          <label htmlFor="number">Line Width: </label>
+          <input
+            type="number"
+            placeholder="5"
+            className="p-2 rounded-md border-black"
+            onChange={(e) => setLineWidth(parseInt(e.target.value))}
+          />
+        </div>
         <ChromePicker color={color} onChange={(e) => setColor(e.hex)} />
         <button
           type="button"
